4b384362685cae0e4a0f08d19ada64407a7f779c
David Deschamps <moreoverlords@gmail.com>
2015-10-16 20:31:28 -0400

    Initial scripts for input parsing, player control

---
+++ Assets/Input/InputParser.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using MidiJack;
+
+public class InputParser : MonoBehaviour
+{
+    public GameObject playerObject;
+    public Player playerScript;
+
+    int MidiMinKey = 0;
+    int MidiMaxKey = 127;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerScript = playerObject.GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        List<Note> notes = new List<Note>();
+
+        for (int i = MidiMinKey; i <= MidiMaxKey; i++)
+        {
+            if (MidiMaster.GetKeyDown(i))
+            {
+                notes.Add(new Note {
+                    Value = i,
+                    Velocity = MidiMaster.GetKey(i)
+                });
+            }
+        }
+    }
+}
---
+++ Assets/Input/Note.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// An occurrence of a key being pressed.
+/// </summary>
+public class Note
+{
+    /// <summary>
+    /// midi value of note, 0-127.
+    /// </summary>
+    public int Value;
+
+    /// <summary>
+    /// force of note press, 0-1.
+    /// </summary>
+    public float Velocity;
+}
+
