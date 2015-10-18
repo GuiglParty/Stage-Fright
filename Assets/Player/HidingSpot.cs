df32448c8d40916f30007130497613177258e565
Dale <putnam.dale17@gmail.com>
2015-10-18 14:17:55 -0400

    Added Hiding Spots

---
+++ Assets/Player/HidingSpot.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HidingSpot : MonoBehaviour {
+    bool occupied;
+    Vector3 returnPosition;
+    Quaternion returnRotation;
+
+    // Use this for initialization
+    void Start()
+    {
+        occupied = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    public void SetReturnPosition(Vector3 pos)
+    {
+        returnPosition = pos;
+    }
+
+    public Vector3 GetReturnPosition()
+    {
+        return returnPosition;
+    }
+
+    public void SetReturnRotation(Quaternion rot)
+    {
+        returnRotation = rot;
+    }
+
+    public Quaternion GetReturnRotation()
+    {
+        return returnRotation;
+    }
+
+
+    public void SetOccupied(bool val)
+    {
+        occupied = val;
+    }
+
+    public bool GetOccupied()
+    {
+        return occupied;
+    }
+}
---
+++ Assets/Player/HidingSpotTrigger.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HidingSpotTrigger : MonoBehaviour {
+    public GameObject hidingSpot;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.GetComponent<Collider>().enabled = true;
+        this.GetComponent<Renderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("hiding spot on trigger stay");
+        if (other.tag == "Player")
+        {
+            Player player = other.GetComponent<Player>();
+            HidingSpot spot = hidingSpot.GetComponent<HidingSpot>();
+
+            if (player.getPlayerState() == PlayerState.lookingAround)
+            {
+                if (!spot.GetOccupied())
+                {
+                    spot.SetReturnPosition(player.transform.position);
+                    spot.SetReturnRotation(player.transform.rotation);
+                    spot.SetOccupied(true);
+                    player.transform.position = spot.transform.position;
+                    player.transform.rotation = spot.transform.rotation;
+                    player.hiding = true;
+                }
+            }
+            else
+            {
+                if (spot.GetOccupied())
+                {
+                    player.transform.position = spot.GetReturnPosition();
+                    player.transform.rotation = spot.GetReturnRotation();
+                    spot.SetOccupied(false);
+                    player.hiding = false;
+                }
+            }
+        }
+    }
+}
