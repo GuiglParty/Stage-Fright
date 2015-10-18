1182ad41ec4d2c4a83aff49b3b1e09884c7692e1
WereDiggle <archdiggle@gmail.com>
2015-10-18 14:10:05 -0400

    dead end trigger for the monster

---
+++ Assets/DeadEndScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeadEndScript : MonoBehaviour {
+	public bool changeMonsterHeading;
+	
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Collider>().enabled = true;
+		changeMonsterHeading = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.tag == "Monster" && changeMonsterHeading) 
+		{
+			MonsterAI monster = other.GetComponent<MonsterAI>();
+			//reverse direction
+			monster.changeHeading(monster.heading*-1);
+			changeMonsterHeading = false;
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Monster") 
+		{
+			changeMonsterHeading = true;
+		}
+	}
+}
